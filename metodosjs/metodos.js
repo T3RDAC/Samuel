1a6a49e2cc55f464975662e29b439815036262fb
T3RDAC <50921950+T3RDAC@users.noreply.github.com>
2021-10-03 16:54:27 -0600

    Add files via upload

---
+++ metodosjs/metodos.js
@@ -0,0 +1,39 @@
+
+var imagenVX = new Array(1) 
+
+
+
+function ajustesV0x()
+{
+    imagenVX=document.getElementById("imgv01");
+    imagenVX.height=100; imagenVX.width=100;
+
+    imagenVX=document.getElementById("imgv02");
+    imagenVX.height=100; imagenVX.width=100;
+
+    imagenVX=document.getElementById("imgv03");
+    imagenVX.height=100; imagenVX.width=100;
+
+    imagenVX=document.getElementById("imgv04");
+    imagenVX.height=100; imagenVX.width=100;
+
+    imagenVX=document.getElementById("imgv05");
+    imagenVX.height=100; imagenVX.width=100;
+document.getel
+}
+
+
+
+
+function AllEvents(){
+
+   // ajustesV0x()
+}
+
+function alInicio()
+{
+ajustesV0x();
+}
+
+window.onload= alInicio;//Metodos que llama cuando incializa
+window.addEventListener=AllEvents;//Metodos Listener
